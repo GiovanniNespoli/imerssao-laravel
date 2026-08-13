eff641b665206afbadda10e59549646065369c35
Giovanni Nespoli <ggnespoli@gmail.com>
2026-08-13 14:42:27 -0300

    feat: add Livewire components for projects index and show
    - Create App\Livewire\Projects\Index and Show components
    - Add their Blade views under resources/views/livewire/projects
    - Wire route views to mount the components via <livewire:.../>
    - Name projects.index route and add projects.show route with
      {project} parameter

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'projects.index');
+Route::view('/', 'projects.index')->name('projects.index');
+Route::view('/project/{project}', 'projects.show')->name('projects.show');
+ 
